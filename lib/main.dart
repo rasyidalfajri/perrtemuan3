f5c71f97fed0bacfcda7f82016d1585832508205
rasyidalfajri <bnggrass@gmail.com>
2024-09-27 09:15:53 +0800

    review3

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            top: 0,
+            child: Container(
+              width: 100,
+              height: 250,
+              margin: const EdgeInsets.only(bottom: 10),
+              color: Colors.red,
+            ),
+          ),
+          Positioned(
+            left: 120,
+            top: 0,
+            child: Container(
+              width: 100,
+              height: 250,
+              margin: const EdgeInsets.only(bottom: 10),
+              color: Colors.blue,
+            ),
+          ),
+          Positioned(
+            left: 240,
+            top: 0,
+            child: Container(
+              width: 100,
+              height: 250,
+              margin: const EdgeInsets.only(bottom: 10),
+              decoration: const BoxDecoration(
+                color: Colors.green, // Pindahkan warna ke dalam BoxDecoration
+                image: DecorationImage(
+                  image: AssetImage("assets/slebew.jpg"),
+                  fit: BoxFit.cover, // Atur ukuran gambar sesuai container
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+  
